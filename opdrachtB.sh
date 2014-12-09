793683095d1188e83b913ce5bfc9948337e25ac6
Sander de Winter <sanderdewinter@gmail.com>
2014-12-09 17:03:09 +0100

    Update opdrachtB.sh

--- opdrachtB.sh
+++ opdrachtB.sh
@@ -1,12 +1,10 @@
-
-#Making sure the user is root, if not prompt user for root access.
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
-#if argument is Jenkins, install Jenkins
-if [ $1 = "Jenkins" ];
+if [ "$(whoami)" != "root" ];
 then
-	wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-	sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-	sudo apt-get update
-	sudo apt-get install jenkins
+	echo "Must be root to run script"
+	exit 1
 fi
+
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get -y install jenkins
