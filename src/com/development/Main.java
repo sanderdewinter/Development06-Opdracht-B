0b02b77eeccfad52e1a292309f985c2efed6aa37
Sander de Winter <sanderdewinter@gmail.com>
2014-12-09 18:56:12 +0100

    Update Main.java

--- src/com/development/Main.java
+++ src/com/development/Main.java
@@ -3,7 +3,7 @@ package com.development;
 public class Main {
 
     public static void main(String[] args) {
-        reken(9,4);
+        reken(9,3);
     }
 
     private static int reken(int getalA,int getalB){
