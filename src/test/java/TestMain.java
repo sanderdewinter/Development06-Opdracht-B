0fd3dbac1bc846cd42a3f02073c7b37ffbfc3636
Sander de Winter <sanderdewinter@gmail.com>
2014-12-09 21:25:46 +0100

    v3

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -4,26 +4,6 @@ import static org.junit.Assert.*;
 public class TestMain {
 
     @Test
-    public void testPasses() {
-        String expected = "Hello, JUnit!";
-        String hello = "Hello, JUnit!";
-        assertEquals(hello, expected);
-    }
-
-    @Test
-    public void testFails() {
-        // The worlds most obvious bug:
-        assertTrue(false);
-    }
-
-    @Test
-    public void testArray() {
-        int [] array1 = new int[] {1, 2, 3};
-        int [] array2 = new int[] {1, 2, 3};
-        assertArrayEquals(array1, array2);
-    }
-
-    @Test
     public void test() {
         int uitkomst = Main.reken(4, 6);
         int expected = 10;
